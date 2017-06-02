04ca5a4b47395eeee6a3968320223792944a9da9
Andy Wu <andy.wu@liferay.com>
2017-06-02 10:10:18 +0800

    change init date id to 1

--- modules/library-listener/library-listener-test/src/main/java/library/listener/test/LibraryListenerTest.java
+++ modules/library-listener/library-listener-test/src/main/java/library/listener/test/LibraryListenerTest.java
@@ -17,22 +17,30 @@ public class LibraryListenerTest implements BundleActivator
     @Override
     public void start( BundleContext context ) throws Exception
     {
-        Repository existingRepo = RepositoryLocalServiceUtil.fetchRepository( 0 );
+        Repository existingRepo = RepositoryLocalServiceUtil.fetchRepository( 1 );
 
         if( existingRepo == null )
         {
-            Repository repository = RepositoryLocalServiceUtil.createRepository( 0 );
+            Repository repository = RepositoryLocalServiceUtil.createRepository( 1 );
             repository.setRepositoryName( "liferay repo" );
             repository.setRepositoryRootUrl(
                 "https://repository.liferay.com/nexus/content/repositories/liferay-public-releases" );
 
             RepositoryLocalServiceUtil.updateRepository( repository );
 
-            Library library = LibraryLocalServiceUtil.createLibrary( 0 );
+            Library library = LibraryLocalServiceUtil.createLibrary( 1 );
             library.setRepositoryId( repository.getPrimaryKey() );
             library.setLibraryGroupId( "com.liferay.portal" );
             library.setLibraryArtifactId( "com.liferay.portal.plugins.sdk" );
             LibraryLocalServiceUtil.updateLibrary( library );
+
+            System.out.println( "Repository id 1 added" );
+            System.out.println( "Library id 1 added" );
+        }
+        else
+        {
+            System.out.println( "Repository id 1 already exist" );
+            System.out.println( "Library id 1 already exist" );
         }
     }
 
